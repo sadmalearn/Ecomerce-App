83bc911e139e9cc86d29ade75d52d7e09e17e5fc
Siddhant Warange <Siddhant Warange@ho.rnt.ai>
2023-11-07 15:46:32 +0530

    Initial frontend code

---
+++ frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route,Navigate } from "react-router-dom";
+import Home from "./User/Home";
+
+export default function main() {
+  return (
+    <section className="mainpage">
+        <Routes>
+        <Route path="/" element={<Navigate to='/main/Home' />} />
+            <Route path="/Home" Component={Home} />
+        </Routes>
+    </section>
+  );
+}
